ef091cdfab85c4331730a7226b7e6fb935aed580
Hayden Young <hayden@hayden.moe>
2025-08-30 19:02:33 +0100

    feat: improve DX of grpc service impl

--- pkg/units/service.go
+++ pkg/units/service.go
@@ -3,22 +3,33 @@ package units
 import (
 	"context"
 
-	corev1 "github.com/milsim-tools/pincer/pkg/api/gen/milsimtools/core/v1"
 	unitsv1 "github.com/milsim-tools/pincer/pkg/api/gen/milsimtools/units/v1"
-	statusproto "google.golang.org/genproto/googleapis/rpc/status"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func (s Units) GetUnit(context.Context, *unitsv1.GetUnitRequest) (*unitsv1.UnitView, error) {
-	return &unitsv1.UnitView{}, status.ErrorProto(&statusproto.Status{
-		Code:    int32(corev1.Code_CODE_UNIMPLEMENTED),
-		Message: "GetUnit is not implemented",
-	})
+func (s *Units) GetUnit(context.Context, *unitsv1.GetUnitRequest) (*unitsv1.UnitView, error) {
+	return &unitsv1.UnitView{}, status.Error(
+		codes.Unimplemented,
+		"GetUnit is not implemented",
+	)
 }
 
-func (s Units) ListUnits(context.Context, *unitsv1.ListUnitsRequest) (*unitsv1.ListUnitsResponse, error) {
-	return &unitsv1.ListUnitsResponse{}, status.ErrorProto(&statusproto.Status{
-		Code:    int32(corev1.Code_CODE_UNIMPLEMENTED),
-		Message: "ListUnits is not implemented",
-	})
+func (s *Units) ListUnits(context.Context, *unitsv1.ListUnitsRequest) (*unitsv1.ListUnitsResponse, error) {
+	resp := &unitsv1.ListUnitsResponse{
+		Units: []*unitsv1.UnitView{
+			{
+				Unit: &unitsv1.Unit{
+					Id: "unit-1",
+					DisplayName: "Unit 1",
+					Slug: "unit-1",
+					Description: "Hello, I am Unit 1",
+					OwnerId: "",
+				},
+			},
+		},
+		NextPageToken: "",
+	}
+
+	return resp, nil
 }
