e7481238e5bc9f47aab55803fe8243892a4b70b1
Hayden Young <hayden@hayden.moe>
2025-09-01 23:36:59 +0100

    fix: update pagination logic for users svc

--- pkg/users/list_users.go
+++ pkg/users/list_users.go
@@ -2,7 +2,6 @@ package users
 
 import (
 	"context"
-	"time"
 
 	"github.com/milsim-tools/pincer/internal/helpers"
 	usersv1 "github.com/milsim-tools/pincer/pkg/api/gen/milsimtools/users/v1"
@@ -12,27 +11,19 @@ import (
 )
 
 func (s *Users) ListUsers(ctx context.Context, req *usersv1.ListUsersRequest) (*usersv1.ListUsersResponse, error) {
-	limit := int(req.PageSize)
-	if limit <= 0 {
-		limit = 50
-	} else if limit > 100 {
-		limit = 100
+	qb := gorm.G[UsersUser](s.db.Db).Select("*")
+	qb = helpers.ApplyPageLimit(qb, int(req.PageSize))
+
+	cursor, err := helpers.CursorFromString(req.PageToken)
+	if err != nil {
+		return &usersv1.ListUsersResponse{}, status.Error(
+			codes.InvalidArgument,
+			"invalid page_token format",
+		)
 	}
 
-	qb := gorm.G[UsersUser](s.db.Db).Limit(limit)
-
-	// TODO: Cursor pagination
-	if req.PageToken != "" {
-		// The user is trying to paginate
-		cursor, err := helpers.CursorFromString(req.PageToken)
-		if err != nil {
-			return &usersv1.ListUsersResponse{}, status.Error(
-				codes.InvalidArgument,
-				"invalid page_token format",
-			)
-		}
-
-		qb.Where("created_at > ?", cursor.CreatedAt)
+	if cursor != nil {
+		qb = helpers.ApplyCursor(qb, cursor)
 	}
 
 	users, err := qb.Find(ctx)
@@ -41,22 +32,6 @@ func (s *Users) ListUsers(ctx context.Context, req *usersv1.ListUsersRequest) (*
 			codes.Internal,
 			"failed to query unit: "+err.Error(),
 		)
-	}
-
-	var pageToken string
-
-	if len(users) > 0 {
-		pc := helpers.PaginationCursor{
-			CreatedAt: users[len(users)-1].CreatedAt.Format(time.RFC3339),
-		}
-
-		pageToken, err = pc.String()
-		if err != nil {
-			return &usersv1.ListUsersResponse{}, status.Error(
-				codes.Internal,
-				"failed to create page_token: "+err.Error(),
-			)
-		}
 	}
 
 	var userViews []*usersv1.UserView
@@ -69,7 +44,7 @@ func (s *Users) ListUsers(ctx context.Context, req *usersv1.ListUsersRequest) (*
 
 	resp := &usersv1.ListUsersResponse{
 		Users:         userViews,
-		NextPageToken: pageToken,
+		NextPageToken: "",
 	}
 
 	return resp, nil
